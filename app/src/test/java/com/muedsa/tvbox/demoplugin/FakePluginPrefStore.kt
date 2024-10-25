2fe0dac0431074f72a437e4c1c05070a91b78727
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-25 15:54:50 +0800

    unit: FakePluginPrefStore update

--- app/src/test/java/com/muedsa/tvbox/demoplugin/FakePluginPrefStore.kt
+++ app/src/test/java/com/muedsa/tvbox/demoplugin/FakePluginPrefStore.kt
@@ -14,7 +14,14 @@ class FakePluginPrefStore : IPluginPerfStore {
     override suspend fun <T> getOrDefault(key: PluginPerfKey<T>, default: T): T =
         store[key.name] as T? ?: default
 
+    override suspend fun filter(predicate: (String) -> Boolean): Map<String, Any> =
+        store.filter { predicate(it.key) }
+
     override suspend fun <T> update(key: PluginPerfKey<T>, value: T) {
         store[key.name] = value as Any
+    }
+
+    override suspend fun <T> remove(key: PluginPerfKey<T>) {
+        store.remove(key.name)
     }
 }
