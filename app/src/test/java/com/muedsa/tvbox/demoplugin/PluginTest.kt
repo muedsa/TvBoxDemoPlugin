c2402b2aafc7d6463930c7bc536f4a7995662b2a
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-25 10:49:40 +0800

    test: onInit_test onLaunched_test

--- app/src/test/java/com/muedsa/tvbox/demoplugin/PluginTest.kt
+++ app/src/test/java/com/muedsa/tvbox/demoplugin/PluginTest.kt
@@ -1,5 +1,6 @@
 package com.muedsa.tvbox.demoplugin
 
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class PluginTest {
@@ -7,6 +8,16 @@ class PluginTest {
     @Test
     fun create_test() {
         TestPlugin
+    }
+
+    @Test
+    fun onInit_test() = runTest {
+        TestPlugin.onInit()
+    }
+
+    @Test
+    fun onLaunched_test() = runTest {
+        TestPlugin.onLaunched()
     }
 
     @Test
